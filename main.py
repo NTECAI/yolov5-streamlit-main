e76e581f2d027b1720fe73a45684aa93869624e7
NTECAI <102501435+NTECAI@users.noreply.github.com>
2022-04-06 16:41:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from io import StringIO
 from pathlib import Path
 import streamlit as st
 import time
-#from detect import detect
-#from layers.functions import Detect
+from detect import detect
 import os
 import sys
 import argparse
